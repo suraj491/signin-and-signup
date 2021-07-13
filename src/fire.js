b44ca901536ceedca15771ec333f9a01b77fbae8
suraj491 <surajroshan491@gmail.com>
2021-07-13 08:38:56 +0530

    first commit

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyAVFLUFg4iyGQCTde2_8rQw9vyVh2LISLs",
+    authDomain: "signin-and-signup-345c5.firebaseapp.com",
+    projectId: "signin-and-signup-345c5",
+    storageBucket: "signin-and-signup-345c5.appspot.com",
+    messagingSenderId: "967861917428",
+    appId: "1:967861917428:web:27ddf191d66a1f0d5330a1"
+  };
+const fire=firebase.initializeApp(firebaseConfig);
+export default fire
